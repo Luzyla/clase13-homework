be5d4ed445797f2e16dd88d2f1f0f0c858c54203
luzyla <luzyla@gmail.com>
2020-08-11 03:31:51 -0300

    Ejercicio 02 - heladería

---
+++ 02-heladeria/main.js
@@ -0,0 +1,12 @@
+//VARIABLES
+let gustoUno = prompt ("Bienvenida, ¿qué gusto de helado vas a elegir primero?");
+let gustoDos = prompt ("El segundo gusto que elegiste ¿cuál es?");
+let gustoTres = prompt ("Tercero y último, ¿cuál te gustaría?");
+
+//ALERT
+alert(`Este es tu helado de ${gustoUno}, ${gustoDos} y ${gustoTres}. Gracias por elegirnos!`)
+
+//console.log
+console.log(gustoUno)
+console.log(gustoDos)
+console.log(gustoTres)
