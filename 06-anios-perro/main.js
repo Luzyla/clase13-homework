1a4c964b85022d6623d149ad9b68970b79ed2470
luzyla <luzyla@gmail.com>
2020-08-11 04:25:38 -0300

    Ejercicio 06 - Años perro.

---
+++ 06-anios-perro/main.js
@@ -0,0 +1,13 @@
+//VARIABLES
+let ageHuman = prompt("¿Cuál es tu edad? Por favor ingresar sólo números!");
+let ageDog = (ageHuman-16)/4
+
+//ALERT
+alert(`Hola! Tus ${ageHuman} años equivalen a ${ageDog} años.
+Nota: Aplica a edades humanas de 16 años en adelante`)
+
+//console.log
+console.log(ageHuman)
+console.log(ageDog)
+
+//comillas mágicas: ``````
