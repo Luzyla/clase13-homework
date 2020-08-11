f2466a295f315a3c5f6b3d0ddfcf49cf9ebe18a0
luzyla <luzyla@gmail.com>
2020-08-11 10:50:48 -0300

    Ejercicio 07 - Minutos a segundos

---
+++ 07-min-a-seg/main.js
@@ -0,0 +1,12 @@
+//VARIABLES
+let min = prompt("Ingresá una cantidad de minutos");
+let seg = min*60
+
+//ALERT
+alert(`${min} minutos equivalen a ${seg} segundos.`)
+
+//console.log
+console.log(min)
+console.log(seg)
+
+//comillas mágicas: ``````
