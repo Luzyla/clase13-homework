6769802ba0f18fa62453579e86a7c713e778d179
luzyla <luzyla@gmail.com>
2020-08-11 03:51:03 -0300

    Ejercicio 04 - Lista de reproducción.

---
+++ 04-lista-reproducción/main.js
@@ -0,0 +1,16 @@
+//VARIABLES
+let playlistName = prompt("Por favor, ponele un nombre a tu nueva playlist:");
+let songOne = prompt("Elegí el nombre de la primera canción:");
+let songTwo = prompt("¿Cuál sería la segunda canción?");
+let songThree = prompt("Tercera y última:");
+
+//ALERT
+alert(`Se ha creado la playlist '${playlistName}' con las canciones '${songOne}', '${songTwo}', '${songThree}'.`);
+
+//console.log
+console.log(playlistName);
+console.log(songOne);
+console.log(songTwo);
+console.log(songThree);
+
+//comillas mágicas: ``````
