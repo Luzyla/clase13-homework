cf43d7c76c4d589d0916fb5c082b6265d1186591
luzyla <luzyla@gmail.com>
2020-08-11 03:40:17 -0300

    Ejercicio 03 - Datos personales

---
+++ 03-datos-personales/main.js
@@ -0,0 +1,16 @@
+//VARIABLES
+let userName = prompt("¿Cuál es tu nombre?");
+let userLastName = prompt("¿Cuál es tu apellido?");
+let userAge = prompt("¿Cuál es tu edad?");
+let userNationality = prompt("¿Cuál es tu nacionalidad?");
+let userDNI = prompt("¿Cuál es tu número de documento?");
+
+//ALERT
+alert(`Nuevo usuario agregado al sistema:
+${userName}
+${userLastName}
+${userAge}
+${userNationality}
+${userDNI}`)
+
+//comillas mágicas: ``````
