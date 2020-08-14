b2426208fd66035b8650126581cde2f07cd50021
luzyla <luzyla@gmail.com>
2020-08-14 13:26:33 -0300

    Ejercicio 10

---
+++ 10-permetro-rectangulo/main.js
@@ -0,0 +1,14 @@
+//VARIABLES
+let base = prompt("Ingresar base del rectángulo")
+let altura = prompt("Ingresar altura del rectángulo")
+let perimetro = (base * 2) + (altura * 2)
+
+//MENSAJE
+alert(`El rectángulo de base ${base} y altura ${altura}, tiene un perímetro de ${perimetro}.`)
+
+//CONSOLE
+console.log(base)
+console.log(altura)
+console.log(perimetro)
+
+//comillas mágicas: ``````
