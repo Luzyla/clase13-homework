43ce8a201a426db233262d151a83ffd41a283a39
luzyla <luzyla@gmail.com>
2020-08-14 13:04:45 -0300

    Ejercicio 08

---
+++ 08-km-a-millas/main.js
@@ -0,0 +1,12 @@
+//VARIABLES
+let km = prompt("Por favor, ingresá la cantidad de kilómetros recorridos:");
+let millas = km * 0.621371;
+
+//MENSAJE
+alert(`Hiciste ${km} kilómetros, que equivalen a ${millas} millas.`)
+
+//CONSOLE
+console.log(km)
+console.log(millas)
+
+//comillas mágicas: ``````
