2222c2d2eacd4f3eecadd4db841fc30184afc3f6
luzyla <luzyla@gmail.com>
2020-08-14 13:14:56 -0300

    Ejercicio 09

---
+++ 09-area-triangulo/main.js
@@ -0,0 +1,14 @@
+//VARIABLES
+let base = prompt("¿Cuánto mide la base del triángulo?");
+let altura =  prompt("¿Cuánto mide la altura del triángulo?");
+let areaT = base * altura;
+
+//MENSAJE
+alert(`El área del triángulo mide ${areaT}.`)
+
+//CONSOLE
+console.log(base)
+console.log(altura)
+console.log(areaT)
+
+//comillas mágicas: ``````
