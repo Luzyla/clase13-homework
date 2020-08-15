8c2ce17fe8fcda161bea3667d9a6c9e498b38c9a
luzyla <luzyla@gmail.com>
2020-08-15 00:34:28 -0300

    Ejercicio 12

---
+++ 12-tpo-viaje/main.js
@@ -0,0 +1,17 @@
+//VARIABLES
+let distancia = prompt("¿Cuál fue la distancia recorrida en km?")
+let aPie = 5
+let enBici = 10
+let enBondi = 20
+let enAuto = 50 
+
+//MENSAJE
+alert(`Recorriste ${distancia}km, equivalen a:
+    
+    ${aPie}km/hora Caminando
+    ${enBici}km/hora en Bici
+    ${enBondi}km/hora en Colectivo
+    ${enAuto}km/hora en Auto.`)
+
+
+//comillas mágicas: ``````
