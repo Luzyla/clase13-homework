1ae652398ebaf9f2e59a3c6ab8ede3d6c2efec93
luzyla <luzyla@gmail.com>
2020-08-15 00:25:20 -0300

    Ejercicio 11: corrección

--- 11-porcentaje/main.js
+++ 11-porcentaje/main.js
@@ -2,10 +2,7 @@
 let numero = prompt("Ingresa el número sobre el que querés calcular el porcentaje:");
 let porcentaje = prompt("Ingresa el porcentaje que queres calcularle al número ingresado:");
 
-let number = Number(numero)
-let porciento = Number(porcentaje)
-
-let calcular = ( 100 * numero ) / porcentaje;
+let calcular = (porcentaje * numero) / 100
 
 //MENSAJE
 alert(`El ${porcentaje}% de ${numero} es ${calcular}`)
